2551c92ef609a816eab2d934a7c65510b817c3e9
Ka-yoko0602 <143280191+Ka-yoko0602@users.noreply.github.com>
2023-09-11 14:58:37 +0900

    課題アップロード

---
+++ kadai_004/Score_Chapter04.java
@@ -0,0 +1,13 @@
+package kadai_004;
+
+public class Score_Chapter04 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		System.out.println("平均点は" + (10 + 20 + 30 + 40 + 50 + 60 + 70 + 80 + 90 + 100) / 10 + "点です。");
+		
+		
+	}
+
+}
